fef023852d4ed62c6b56aab0280dbc00cc318669
Steve Lam <stevyo99@yahoo.ca>
2014-07-04 14:41:50 -0700

    c/p about us page from old site

---
+++ pages/about.php
@@ -0,0 +1,47 @@
+<div class="content-wrapper">
+    <div class="main-content">
+        <div class="post">
+            Both boys and girls are welcome to come and join our program.<br><br>
+            <table border="0">
+                <tr>
+                    <td>Group Chair<br>
+                        Group Commissioner<br>
+                        Group Registrar
+                    </td><td>:<br>:<br>:</td><td>
+                        Mr. Arthur Li<br>
+                        Mr. Jackson Li<br>
+                        Ms. Lilian Chu
+                    </td><td>
+                        (sikwing@hotmail.com)<br>
+                        (jackson.li@pccrovers.com)<br>
+                        (lc3190@gmail.com)
+                    </td></tr></table>
+            <br>
+            <div>
+                <h3>Location:</h3>
+                <a href="http://hastingscommunitycentre.ca/">Hasting Community Center</a><br>
+                3096 East Hastings Street, Vancouver B.C. V5K 2A3<br><br>
+                <h3>Time of regular meetings:</h3>
+                Saturdays 2:30pm to 4:00pm (Beaver Section)<br>
+                Saturdays 2:30pm to 4:30pm (All Other Sections)<br><br>
+                <h3>Section Structure</h3>
+                <table><tr><td>
+                            Beavers<br>
+                            Cubs<br>
+                            Scouts<br>
+                            Ventures<br>
+                            Rovers<br>
+                        </td><td>:<br>:<br>:<br>:<br>:<br></td><td>
+                            5 - 7 Years old<br>
+                            8 - 10 Years old<br>
+                            11 - 14 Years old<br>
+                            14 - 17 Years old<br>
+                            18 - 26 Years old<br></td></tr></table>
+            </div>
+        </div>
+    </div>
+    <div class="right-side">
+        there is something here
+    </div>
+    <div style="clear: both;"></div>
+</div>
