5819f0a16c6bb0e4593b89295e0d3d3240b9d314
Steve Lam <stevyo99@yahoo.ca>
2014-07-03 18:48:43 -0700

    minified fb init script

--- js/fbinit.js
+++ js/fbinit.js
@@ -1,5 +1,1 @@
-var ACCESS_TOKEN = "524905380971501|vwvIt1ualZz_1V5DX17el6NJPe0";
-FB.init({
-    version: 'v2.0',
-    appId: 524905380971501
-});
+var ACCESS_TOKEN="524905380971501|vwvIt1ualZz_1V5DX17el6NJPe0";FB.init({version:'v2.0',appId:524905380971501});
