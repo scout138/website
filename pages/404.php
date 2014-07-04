1b7433c7ac2722b4006196401775077f595ab03e
Steve Lam <stevyo99@yahoo.ca>
2014-07-04 01:47:58 -0700

    added 404 page

---
+++ pages/404.php
@@ -0,0 +1,18 @@
+<div class="content-wrapper">
+    <div style="width:700px;margin: 0 auto;">
+        <img src="/images/campfire.png" style="width: 300px;float: left;padding: 10px;"/>
+        <div style="float: left;padding: 10px;">
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div style="font-size: 2em;">Oh No!</div>
+            <br/>
+            <br/>
+            <p>Whatever you thought was here seems to be gone...</p>
+        </div>
+        <div style="clear: both;"></div>
+    </div>
+</div>
