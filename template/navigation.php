58759efa9e19fed1694e970dca4ed39e04818ac8
Steve Lam <stevyo99@yahoo.ca>
2014-07-04 01:41:46 -0700

    selected nav item now has the same effect has hovered

--- template/navigation.php
+++ template/navigation.php
@@ -1,10 +1,15 @@
+<?php
+function getNavClass($p) {
+    return ($_GET["page"] == $p ? " class=\"selected\"" : "");
+}
+?>
 <div class="header">
     <div class="logo"></div>
     <h2 class="title">138 Scout Group</h2>
     <ul class="nav">
-        <li><a href="javascript:void(0);">Home</a></li>
-        <li><a href="javascript:void(0);">About Us</a></li>
-        <li><a href="javascript:void(0);">Calendar</a></li>
-        <li><a href="javascript:void(0);">Registration</a></li>
+        <li><a href="javascript:void(0);"<?=getNavClass("home");?>>Home</a></li>
+        <li><a href="javascript:void(0);"<?=getNavClass("about");?>>About Us</a></li>
+        <li><a href="javascript:void(0);"<?=getNavClass("calendar");?>>Calendar</a></li>
+        <li><a href="javascript:void(0);"<?=getNavClass("registration");?>>Registration</a></li>
     </ul>
 </div>
