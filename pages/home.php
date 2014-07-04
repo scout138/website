678d60078100b1da03f345ab6ddd8864856ffcfb
Steve Lam <stevyo99@yahoo.ca>
2014-07-03 18:46:48 -0700

    move FB initiation stuff to another script

--- pages/home.php
+++ pages/home.php
@@ -7,13 +7,7 @@
     <div style="clear: both;"></div>
 </div>
 <script src="//connect.facebook.net/en_US/sdk.js"></script>
-<script>
-    var ACCESS_TOKEN = "524905380971501|vwvIt1ualZz_1V5DX17el6NJPe0";
-    FB.init({
-        version: 'v2.0',
-        appId: 524905380971501
-    });
-</script>
+<script src="js/fbinit.js"></script>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="js/isotope.pkgd.min.js"></script>
