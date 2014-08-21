e0fd8f99dfbae2bb1b2979bce8f2511a0dbff3fe
Steve Lam <stevyo99@yahoo.ca>
2014-08-21 01:50:02 -0700

    layout overhaul

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,8 @@
 
 	<link href="../css/lightview.css" rel="stylesheet" type="text/css">
 
+	<link href="../css/jquery.jgrowl.min.css" rel="stylesheet" type="text/css">
+
 	<link rel="stylesheet" href="style.css"/>
 
 	<script src="//connect.facebook.net/en_US/sdk.js"></script>
@@ -26,6 +28,8 @@
 	<script type="text/javascript" src="../js/lightview/lightview.js"></script>
 	<script type="text/javascript" src="../js/spinners/spinners.min.js"></script>
 	<script type="text/javascript" src="../js/lightview/lightview.js"></script>
+
+	<script type="text/javascript" src="../js/jquery.jgrowl.min.js"></script>
 
 	<script type="text/javascript" src="../js/isotope.pkgd.min.js"></script>
 
@@ -46,56 +50,59 @@
 
 </script>
 <body>
-	<div id = "header">
-		<div id = "logo">
-			<img src = "http://www.scouts.ca/brandcentre/resources/logo/scouts-logo-notagline-web.png" alt = "logo" height = "144px"  width = "180px">
-		</div>
-		<div id = "title">138 Scout Group Control Panel</div>
-	<br style="clear:both;"/><hr />
-	</div>
-		<div id = "menu">
+<table id="structure" width="100%" cellpadding="0" cellspacing="0">
+	<tr>
+		<td colspan="2" id="header" valign="bottom">
+			<div id="logo"></div>
+			<div id="title"></div>
+		</td>
+	</tr>
+	<tr>
+		<td id="menu" valign="top">
 			<ul>
-				<li id = "menuItems" onclick = "showHide('create')"><a href = "#">Create Post</a></li>
-				<li id = "menuItems" onclick = "showHide('edit')"><a href = "#">Edit Post</a></li>
-				<li id = "menuItems" onclick = "showHide('calender')"><a href = "#">Calender</a></li>
-				<li id = "menuItems" ><a href = "php/logoff.php">Log Off</a></li>
+				<li><a href="javascript: showHide('#create');">Create Post</a></li>
+				<li><a href="javascript: showHide('#edit');">Edit Post</a></li>
+				<li><a href="javascript: showHide('#calendar');">Calendar</a></li>
+				<li><a href="php/logoff.php">Log Off</a></li>
 			</ul>
-		</div>
-		<div id = "content">
-			<div id = "create"><h1>Create Post</h1>
-				<div id = "error"></div>
-				<form name="input" action="#" method="post">
-					Header:<br />
-					<input type = "text" name = "header" id = "postHeader"><br />
-					Facebook Photo Album:<br />
-					<input type = "text" name = "album" id = "album" pattern = "\d*"> <a href="javascript: fbAlbumPicker();">pick</a><br />
-					Additional Details:<br />
-					<textarea rows = "50" cols = "100" name = "description" id = "description"></textarea><br />
-				</form>
-				<button value = "Create" onclick = "submit()">Create Post</button>
-				<button value = "Preview Post" onclick = "preview()">Preview Post</button>
-				<h1>Preview</h1>
-				<div id = "preview"></div>
-			</div>
+		</td>
+		<td id="content" valign="top">
+			<div id="scroll-wrap">
+				<div id="create"><h1>Create Post</h1>
+					<div id="error"></div>
+					<form name="input" action="#" method="post">
+						Title:<br />
+						<input type = "text" name = "header" id = "postHeader"><br />
+						Facebook Photo Album:<br />
+						<input type = "number" name = "album" id = "album"> <a href="javascript: fbAlbumPicker();">pick</a><br />
+						Additional Details:<br />
+						<textarea rows = "50" cols = "100" name = "description" id = "description"></textarea><br />
+					</form>
+					<button value = "Create" onclick = "submit()">Create Post</button>
+					<button value = "Preview Post" onclick = "preview()">Preview Post</button>
+					<h1>Preview</h1>
+					<div id="preview"></div>
+				</div>
 
-			<div id="album-selector" style="display:none;">
-				<h1>Please pick an album</h1>
-				<div id="masonr">
+				<div id="album-selector" style="display:none;">
+					<h1>Please pick an album</h1>
+					<div id="masonr">
 
+					</div>
+				</div>
+
+				<div id="edit" style="display: none;"><h1 style="display:inline-block;margin-right: 15px;">Edit Post</h1><a href="javascript: grabPosts()">refresh</a>
+					<div id="editPosts">
+					</div>
+				</div>
+				<div id="calendar" style="display: none;"><h1>Calendar</h1>
+					<iframe src="https://www.google.com/calendar/embed?src=pccrovers.com_pojeic2sd1ojijt7ohop7gt338%40group.calendar.google.com&ctz=America/Vancouver" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no">
+					</iframe>
 				</div>
 			</div>
-			
-			<div id = "edit"><h1>Edit Post</h1> 
-				<div id = "editPosts">
-				</div>
-				<div id = "pagination">
-				</div>
-			</div>
-			<div id = "calender"><h1>Calender</h1>
-				<iframe src="https://www.google.com/calendar/embed?src=pccrovers.com_pojeic2sd1ojijt7ohop7gt338%40group.calendar.google.com&ctz=America/Vancouver" style="border: 0" width="80%" height="600" frameborder="0" scrolling="no">
-				</iframe>
-			</div>
-		</div>
+		</td>
+	</tr>
+</table>
 
 <div id="spinner" style="display: none;">
 
