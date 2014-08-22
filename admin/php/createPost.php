8b39833d2fae43998752a587352536a624729fa7
Steve Lam <stevyo99@yahoo.ca>
2014-08-21 23:56:39 -0700

    rewrote createPost.php

--- admin/php/createPost.php
+++ admin/php/createPost.php
@@ -1,43 +1,39 @@
 <?php
-	require('../../php/config.php');
-	if($_POST['header'] == "" || !isset($_POST['header'])) {
-		echo "You need to have a header";
-	} else {
-		$con=mysqli_connect("$host","$user","$password","$db");
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-		if (mysqli_connect_errno()) {
-		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		}
-		$description = "";
-		$header = $_POST['header'];
-		$albumID = "";
-		if(isset($_POST['album']) && $_POST['album'] != "") {
-			$album = explode(".",$_POST['album'],10);
-			
-			$count = 0;
-			foreach($album as $value) {
-				if(is_numeric($value) && $albumID == "") {
-					$albumID = $album[$count];
-				}
-				$count++;
-			}
-		} else {
-			$i = 0;
-			$album[$i] = "";
-		}
-		//echo $albumID;
-		if(isset($_POST['description'])) {
-			$description = $_POST['description'];
-		} else {
-			echo "You need to put a description";
-		}
-		$request = "INSERT INTO post (heading, albumID, description) VALUES ('$header', '$albumID', '$description')";
-		//echo $request;
-		if(mysqli_query($con, $request)) {
-			echo "Successful.";
-		} else {
-			echo "Failed";
-		}
-		mysqli_close($con);
-	}	
-?>
+if(!isset($_POST['header']) || $_POST['header'] == "")
+	die("Title is missing");
+
+if(!isset($_POST['album']) || $_POST['album'] == "")
+	die("Facebook album is missing");
+
+if(!isset($_POST['description']) || $_POST['description'] == "")
+	die("Description is missing");
+
+// connect to the database
+require('../../php/config.php');
+$con = mysqli_connect("$host","$user","$password","$db");
+if (mysqli_connect_errno())
+  die("Failed to connect to MySQL: " . mysqli_connect_error());
+
+// escape the user input to prevent sql injection attacks!
+$header = mysqli_real_escape_string($con, $_POST['header']);
+$albumID = mysqli_real_escape_string($con, $_POST['album']);
+$description = mysqli_real_escape_string($con, $_POST['description']);
+
+// validate album id
+$album_data = file_get_contents("http://graph.facebook.com/" . $albumID ."?pretty=0");
+$album_data = json_decode($album_data, true);
+if(isset($album_data["error"]) || !isset($album_data["from"]))
+	die("That Facebook album does not exist");
+if($album_data["from"]["id"] != "292891094182467")
+	die("We do not own that Facebook album");
+
+$request = "INSERT INTO post (heading, albumID, description) VALUES ('$header', '$albumID', '$description')";
+
+if(!mysqli_query($con, $request))
+	die(mysqli_error($con));
+
+echo "ok";
+mysqli_close($con);
