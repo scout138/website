28105137e757a513cfa646a561ea34ce0fb3e6e3
Steve Lam <stevyo99@yahoo.ca>
2014-08-22 19:23:29 -0700

    added pretty print option

--- json.php
+++ json.php
@@ -2,6 +2,7 @@
 
 $_POST["limit"] = (isset($_POST["limit"]) ? $_POST["limit"] : 5);
 $_POST["page"] = (isset($_POST["page"]) ? $_POST["page"] : 0);
+$_POST["pretty"] = (isset($_POST["pretty"]) ? $_POST["pretty"] : 0);
 
 require("php/config.php");
 $link = mysqli_connect($host, $user, $password, $db);
@@ -26,6 +27,9 @@ if($result = mysqli_query($link, "SELECT id, heading, albumID, description, time
 if(count($return_array) >= $_POST["limit"])
 	$return_array["nextPage"] = $_POST["page"] + 1;
 
-echo json_encode($return_array);
+if($_POST["pretty"] == 1)
+	echo json_encode($return_array, JSON_PRETTY_PRINT);
+else
+	echo json_encode($return_array);
 
 mysqli_close($link);
