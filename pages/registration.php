369c6ae02421d56d8ccc0d1e81c341801ee76ca6
Steve Lam <stevyo99@yahoo.ca>
2014-09-08 01:32:12 -0700

    added registration page

---
+++ pages/registration.php
@@ -0,0 +1,102 @@
+<div class="content-wrapper">
+    <div class="main-content">
+	    <div class="post">
+		    <div class="title">Registration</div>
+		    <div class="words">
+			    <table style="text-align: center; width: 100%">
+				    <thead style="text-decoration: underline; ">
+				    <tr>
+					    <td>
+					    </td>
+					    <td>
+						    Scouts Canada
+					    </td>
+					    <td>
+						    Group Fee
+					    </td>
+					    <td>
+						    Total
+					    </td>
+				    </tr>
+				    </thead>
+				    <tbody>
+				    <tr>
+					    <td align="left">
+						    Returning member (before June 30)
+					    </td>
+					    <td>
+					        $155
+					    </td>
+					    <td>
+						    Waived
+					    </td>
+					    <td>
+						    $155
+					    </td>
+				    </tr>
+				    <tr>
+					    <td align="left">
+						    Returning member (after June 30)
+					    </td>
+					    <td>
+						    $175
+					    </td>
+					    <td>
+						    $20
+					    </td>
+					    <td>
+						    $195
+					    </td>
+				    </tr>
+				    <tr>
+					    <td align="left">
+						    New member (before June 30)
+					    </td>
+					    <td>
+						    $155
+					    </td>
+					    <td>
+						    $20
+					    </td>
+					    <td>
+						    $175
+					    </td>
+				    </tr>
+				    <tr>
+					    <td align="left">
+						    New member (after June 30)
+					    </td>
+					    <td>
+						    $175
+					    </td>
+					    <td>
+						    $20
+					    </td>
+					    <td>
+						    $195
+					    </td>
+				    </tr>
+				    <tr>
+					    <td align="left">
+						    3<sup>rd</sup> & 4<sup>th</sup> children of the same family
+					    </td>
+					    <td>
+						    Free
+					    </td>
+					    <td>
+						    Waived
+					    </td>
+					    <td>
+						    Free
+					    </td>
+				    </tr>
+				    </tbody>
+			    </table>
+		    </div>
+	    </div>
+    </div>
+    <div class="right-side">
+        there is something here
+    </div>
+    <div style="clear: both;"></div>
+</div>
