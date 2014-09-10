500522a1edc9e2368acefaa8c01afdc01ef0ff72
Steve Lam <stevyo99@yahoo.ca>
2014-09-09 21:42:59 -0700

    changed registration page

--- pages/registration.php
+++ pages/registration.php
@@ -20,12 +20,12 @@
 				    </tr>
 				    </thead>
 				    <tbody>
-				    <tr>
+				    <!--tr>
 					    <td align="left">
 						    Returning member (before June 30)
 					    </td>
 					    <td>
-					        $155
+						    $155
 					    </td>
 					    <td>
 						    Waived
@@ -61,10 +61,10 @@
 					    <td>
 						    $175
 					    </td>
-				    </tr>
+				    </tr-->
 				    <tr>
 					    <td align="left">
-						    New member (after June 30)
+						    New member
 					    </td>
 					    <td>
 						    $175
@@ -78,7 +78,7 @@
 				    </tr>
 				    <tr>
 					    <td align="left">
-						    3<sup>rd</sup> & 4<sup>th</sup> children of the same family
+						    3<sup>rd</sup> & 4<sup>th</sup> member of the same family
 					    </td>
 					    <td>
 						    Free
@@ -92,11 +92,57 @@
 				    </tr>
 				    </tbody>
 			    </table>
+			    <sub>*Cheques to be written to "138th East Vancouver Scout Group"</sub><br>
+			    <sub>**Cheques and registration forms can be given to the leader of your section</sub>
+		    </div>
+	    </div>
+	    <div class="post">
+		    <div class="title">Uniform Requirements</div>
+		    <div class="words">
+			    <table style="text-align: left; width: 100%">
+				    <thead style="text-decoration: underline; ">
+					    <tr>
+						    <td>Section</td>
+						    <td>Articles Required</td>
+						    <td>Total Cost (tax not included)</td>
+					    </tr>
+				    </thead>
+				    <tbody>
+					    <tr>
+						    <td>Beavers</td>
+						    <td>Beaver Hat + Beaver Vest</td>
+						    <td>$29.98</td>
+					    </tr>
+					    <tr>
+						    <td>Cubs</td>
+						    <td>Gray Cub Polo + Navy Active Pants</td>
+						    <td>$51.98</td>
+					    </tr>
+					    <tr>
+						    <td>Scouts</td>
+						    <td>Green Scout Shirt + Tan Pants</td>
+						    <td>$53.98 - 59.98</td>
+					    </tr>
+					    <tr>
+						    <td>Venturers</td>
+						    <td>Navy Scout Shirt + Leather Belt + Tan Pants</td>
+						    <td>$89.97 - 95.97</td>
+					    </tr>
+				    </tbody>
+			    </table>
+			    <sub>*Uniform can only be purchased at Scout House Canada (<a href="https://www.google.ca/maps/place/Scouts+Canada,+664+Broadway+W,+Vancouver,+BC+V5Z/">664 Broadway W Vancouver, BC</a>)</sub><br>
+			    <sub>**Please make sure to call in (604-879-5721) before to make sure they have uniform in stock as they tend to sell out once in awhile.</sub>
 		    </div>
 	    </div>
     </div>
     <div class="right-side">
-        there is something here
+	    <div class="widget">
+		    <div class="title">
+			    Download Forms
+		    </div>
+		    <a href="#">Participant Registration Form</a><br>
+		    <a href="#">Volunteer Registration Form</a>
+	    </div>
     </div>
     <div style="clear: both;"></div>
 </div>
