97703605d637dfab5f03442499e33325ec88009e
Steve Lam <stevyo99@yahoo.ca>
2015-09-18 11:51:51 -0700

    updated navigation

--- template/navigation.php
+++ template/navigation.php
@@ -2,9 +2,28 @@
 function make_nav() {
     $nav = array(
         "Calendar" => BASE_URL . "calendar",
-        "Leaders' Resources" => array(
-            "Reimbursement Form" => "//files.scout138.com/leader-reimbursement-form-2014.pdf",
+        "Resources" => array(
+            "Reimbursement Form" => "//files.scout138.com/leader-reimbursement-form-2014.pdf?dl",
+            "Canadian Path" => [
+                "Beavers" => "//files.scout138.com/canadian-path/beavers.pdf?dl",
+                "Cubs" => "//files.scout138.com/canadian-path/cubs.pdf?dl",
+                "Scouts" => "//files.scout138.com/canadian-path/scouts.pdf?dl",
+                "Venturers" => "//files.scout138.com/canadian-path/venturers.pdf?dl",
+                "FAQ" => "//files.scout138.com/canadian-path/faq.pdf?dl"
+            ],
+            "Tech Guide" => "//sites.google.com/a/scout138.com/wiki/",
+            "Attendance" => "http://attendance.scout138.com/",
+            "Leaders' Contact Info" => "#",
+            "Physical Fitness Forms" => [
+                "Beavers" => "//files.scout138.com/physical-fitness-forms/beavers/",
+                "Cubs" => "//files.scout138.com/physical-fitness-forms/cubs/",
+                "Scouts" => "//files.scovut138.com/physical-fitness-forms/scouts/",
+                "Venturers" => "//files.scout138.com/physical-fitness-forms/venturers/",
+            ],
+            "Uniform Badge Placement" => "//files.scout138.com/public/insignia-placement.pdf",
+            "Police Record Check" => "//justice.gov.bc.ca/eCRC/",
         ),
+        "Thank a Leader!" => "//www.myscouts.ca/ca/commendation/submit",
         "Registration" => BASE_URL . "registration",
         "About Us" => BASE_URL . "about",
     );
